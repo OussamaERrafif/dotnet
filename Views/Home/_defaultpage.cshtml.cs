64847fbfdfe174c008e0368cec4eb559eb613ab5
OussamaERrafif <oussamaerra2002@gmail.com>
2023-12-12 21:42:50 +0100

    3

---
+++ Views/Home/_defaultpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projetdotnet.Views.Home
+{
+    public class _defaultpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
