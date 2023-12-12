f53f9851db3b14d743fabaff72960b6925588a2d
OussamaERrafif <oussamaerra2002@gmail.com>
2023-12-12 21:22:40 +0100

    n

---
+++ Controlers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult LoadParagraph(string id)
+    {
+        return PartialView($"~/Views/Home/Paragraphe{id}.cshtml");
+    }
+
+    public IActionResult LoadQuiz(string id)
+    {
+        return PartialView($"~/Views/Home/Quiz{id}.cshtml");
+    }
+
+    
+    public IActionResult LoadDefaultPage()
+    {
+        return PartialView("~/Views/Home/_defaultpage.cshtml");
+    }
+}
